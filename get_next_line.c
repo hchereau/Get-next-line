5a9f608ac19b50b86d2bcc2c87e3a5a0edc00635
Hugo CHEREAU <hchereau@e1r7p5.clusters.42paris.fr>
2023-01-08 17:00:42 +0100

    process has been finish

--- get_next_line.c
+++ get_next_line.c
@@ -6,13 +6,44 @@
 /*   By: hchereau <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/08 15:18:40 by hchereau          #+#    #+#             */
-/*   Updated: 2023/01/08 15:23:23 by hchereau         ###   ########.fr       */
+/*   Updated: 2023/01/08 16:59:49 by hchereau         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int main(void)
+
+
+
+
+
+int main(int argc, char **argcv)
 {
-	open
+	int	fd;
+	int i;
+
+	fd = open("readthis.txt", O_RDONLY);
+	i = 0;
+	while (i <= argcv[1])
+	{
+		get_next_line(fd);
+		++i;
+	}
 }
+
+/*
+process:
+
+	ajouter le static BUFFER de la ligne precedent au debut de la str final
+	free la ligne precedente
+	read sur BUFFERR size
+	parcourir BUFFER jusqu'a tomber sur \n
+	si pas de \n:
+		join la str final et le BUFFER sur une nouvelle ligne
+		free la ligne precedente
+	si \n:
+		ajouter \n a str final
+		garder en static suite BUFFER
+	
+	
+*/
