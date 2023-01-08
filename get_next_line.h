930974a6cdcaf57671062ebf40ad3bc6435b55e1
Hugo CHEREAU <hchereau@e1r7p5.clusters.42paris.fr>
2023-01-08 15:23:46 +0100

    start_gnl

---
+++ get_next_line.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hchereau <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/08 15:03:15 by hchereau          #+#    #+#             */
+/*   Updated: 2023/01/08 15:20:55 by hchereau         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <fcntl.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
+
+
+
+#endif
